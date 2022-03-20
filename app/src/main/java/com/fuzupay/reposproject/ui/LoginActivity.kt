8840532299e2f36898e43a4c69ac2a26390b5f2a
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-03-20 21:33:35 +0300

    created login Activity and added google login firebase dependencies

--- app/src/main/java/com/fuzupay/reposproject/ui/LoginActivity.kt
+++ app/src/main/java/com/fuzupay/reposproject/ui/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.fuzupay.reposproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fuzupay.reposproject.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
